ae97aa45d1477c3ce799df63e583e9d5c51258d9
Kendall Jackson <kendallkj00@gmail.com>
2024-02-13 14:10:30 -0500

    fix: new marketing images, typo fixes

--- components/NewCard.vue
+++ components/NewCard.vue
@@ -1,21 +1,24 @@
 <template>
-    <div id="card" class="relative mx-auto sm:m-4">
-        <!--outline background maybe match height idk -->
-          <div id="outline" class="bg-gradient-to-tr from-red-500 to-red-700 absolute rounded-md h-72 w-72 -right-4 -bottom-2"></div>
-        <!-- content -->
-          <div id="content" class="p-8 w-80 h-80 bg-gray-50 rounded-md shadow-lg relative space-y-7" >
-              <!-- <div class="bg-gradient-to-tr from-red-400 to-red-600 h-3 w-16 ml-auto"></div> -->
-							<slot></slot>
-              <span class="text-2xl font-bold text-gray-800">{{ heading }}</span>
-							<div class="h-0.5 absolute top-20 rounded-md w-16 bg-red-600/65" />
-              <p class="text-gray-600">
-                {{ copy }}
-              </p>
-                  <a class="flex items-center" href="#"><span>Learn More </span> <svg class="w-4 h-4 ml-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                  </svg></a>
-          </div>
-      </div>
+  <div id="card" class="relative mx-auto sm:m-4">
+    <!--outline background maybe match height idk -->
+    <div id="outline" class="bg-gradient-to-tr from-red-500 to-red-700 absolute rounded-md h-72 w-72 -right-4 -bottom-2"></div>
+    <!-- content -->
+    <div id="content" class="p-8 w-80 h-80 bg-gray-50 rounded-md shadow-lg relative space-y-7">
+      <!-- <div class="bg-gradient-to-tr from-red-400 to-red-600 h-3 w-16 ml-auto"></div> -->
+      <slot></slot>
+      <span class="text-2xl font-bold text-gray-800">{{ heading }}</span>
+      <div class="h-0.5 absolute top-20 rounded-md w-16 bg-red-600/65" />
+      <p class="text-gray-600">
+        {{ copy }}
+      </p>
+      <a class="flex items-center" href="#">
+        <span>Learn More </span>
+        <svg class="w-4 h-4 ml-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+        </svg>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
--- components/Testimonial.vue
+++ components/Testimonial.vue
@@ -23,7 +23,7 @@
           </figure>
         </div>
       </div>
-      <img src="https://images.unsplash.com/photo-1629976040712-4df9f2ff7205?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Product screenshot" class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:ml-0 h-[33.75rem]" width="2432" height="1442">
+      <img src="/broken.png" alt="Broken Audi Windshield" class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:ml-0 h-[33.75rem]" width="2432" height="1442">
     </div>
   </div>
 </div>
