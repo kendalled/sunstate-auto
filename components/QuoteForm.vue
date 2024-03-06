4616a65e382c42080c5cd4f836a0a0b7d1583dbb
Kendall Jackson <kendallkj00@gmail.com>
2024-03-06 16:45:35 -0500

    fix: try handler again

--- components/QuoteForm.vue
+++ components/QuoteForm.vue
@@ -381,7 +381,7 @@
           </div>
         </div>
         <div class="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
-          <button type="submit" class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Submit Forms</button>
+          <button type="submit" @click.prevent="submitHandler()" class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Submit Forms</button>
         </div>
       </section>
     </div>
