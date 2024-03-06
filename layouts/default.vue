d948f6c96f2680de71012349af1a4f10f0bddf20
Kendall Jackson <kendallkj00@gmail.com>
2024-03-06 16:16:09 -0500

    fix: remove routerules, fix caching

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,11 @@
   <div>
     <DarkBanner />
     <Navigation />
+    <head>
+      <Meta name="cache-control" content="no-cache, no-store, must-revalidate" />
+      <Meta name="pragma" content="no-cache" />
+      <Meta name="expires" content="0" />
+    </head>
     <slot />
     <LightFooter />
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  routeRules: {
-    '/agent': { prerender: true },
-  },
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-aos',
