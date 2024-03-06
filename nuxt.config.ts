af2761708aeeb589f81cc8254e4e969e0c408e61
Kendall Jackson <kendallkj00@gmail.com>
2024-03-06 15:50:51 -0500

    fix: prerender agent form

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  routeRules: {
+    '/agent': { prerender: true },
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-aos',
