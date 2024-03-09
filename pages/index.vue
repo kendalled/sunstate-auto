d0bd9148d4bbe52efb24c1566e4d02c585ed227b
Kendall Jackson <kendallkj00@gmail.com>
2024-03-09 15:42:41 -0500

    fix: meta title and desc.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
   <main>
+    <Head>
+      <Title>Sunstate AutoGlass: Florida's Clear Choice for Auto Glass.</Title>
+      <Meta name="description" content="Trust SunState Auto Glass for top-notch windshield repair since 2007, proudly serving Orlando and Tampa. Our experienced team ensures swift, same-day service, prioritizing your safety on the road. Choose SunState for quality repairs and unmatched expertise in the Central Florida region."/>
+    </Head>
     <PictureHero />
 		<TimelineFeat />
     <Testimonial flavor="Same-day service" heading="Mobile service you can trust." copy="Our team of experts are here to provide you with the windshield repairs and replacements you need. With our fast and reliable mobile service, getting back on the road couldn't be easier." test="I recently got my windshield replaced by Sunstate Autoglass. The service was quick and professional, with easy scheduling. Overall, a great experience — would highly recommend them for anyone needing a windshield replacement." reviewer="Maria Hill" img="/broken.png" imgAlt="Broken Audi Windshield"/>
