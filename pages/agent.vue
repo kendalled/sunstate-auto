11bcb704b5862d8f703aab1bdbedf810e525ccc8
Kendall Jackson <kendallkj00@gmail.com>
2024-03-21 16:02:23 -0400

    fix: swap agent & agent2

--- pages/agent.vue
+++ pages/agent.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="bg-gray-100">
     <DarkHeader />
-    <nuxt-link to="/agent2" title="Alternate form" class="ml-[19.5rem] text-blue-500 underline">Form Not Working?</nuxt-link>
     <!-- <StackedQuote /> -->
-    <QuoteForm />
+    <AltQuote />
   </div>
 </template>
--- pages/agent2.vue
+++ pages/agent2.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="bg-gray-100">
     <DarkHeader />
+    <nuxt-link to="/agent2" title="Alternate form" class="ml-[19.5rem] text-blue-500 underline">Form Not Working?</nuxt-link>
     <!-- <StackedQuote /> -->
-    <AltQuote />
+    <QuoteForm />
   </div>
 </template>
