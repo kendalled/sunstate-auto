8eb1d822811389434eaf35cbc1e91da5716e81dd
Kendall Jackson <kendallkj00@gmail.com>
2024-03-27 17:37:21 -0400

    fix: update input labels

--- components/AltQuote.vue
+++ components/AltQuote.vue
@@ -26,7 +26,7 @@
               </div>
   
               <div class="sm:col-span-3">
-                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Customer Email</label>
                 <div class="mt-2">
                   <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6">
                 </div>
@@ -323,7 +323,7 @@
               </div>
   
               <div class="col-span-3">
-                <label for="agent-email" class="block text-sm font-medium leading-6 text-gray-900">Agent Email Address</label>
+                <label for="agent-email" class="block text-sm font-medium leading-6 text-gray-900">Agent / Dealer Email</label>
                 <div class="mt-2">
                   <input id="agent-email" name="agent-email" type="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6">
                 </div>
