0da5f4d2c3c6d810daf5765eec3856b4938f4ff5
Kendall Jackson <kendallkj00@gmail.com>
2024-06-13 15:54:33 -0400

    fix: hover styles

--- components/NewCard.vue
+++ components/NewCard.vue
@@ -11,7 +11,8 @@
       <p class="text-gray-600">
         {{ copy }}
       </p>
-      <nuxt-link class="flex items-center" :title="heading" :to="'/services#' + extension">
+      <!-- todo: color theory -->
+      <nuxt-link class="flex items-center text-gray-700 hover:text-gray-800 hover:underline" :title="heading" :to="'/services#' + extension">
         <span>Read More </span>
         <svg class="w-4 h-4 ml-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
