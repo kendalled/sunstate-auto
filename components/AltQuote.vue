154f772089bd7dc834132b43c5baf5904e791e27
Kendall Jackson <kendallkj00@gmail.com>
2024-06-17 16:34:10 -0400

    fix: make insurance carrier a required field

--- components/AltQuote.vue
+++ components/AltQuote.vue
@@ -143,7 +143,7 @@
               <div class="sm:col-span-full">
                 <label for="carrier-name" class="block text-sm font-medium leading-6 text-gray-900">Insurance Carrier Name</label>
                 <div class="mt-2">
-                  <input type="text" name="carrier-name" id="carrier-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6">
+                  <input required type="text" name="carrier-name" id="carrier-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6">
                 </div>
               </div>
   
